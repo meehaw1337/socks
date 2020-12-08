d9c1cf10ddf1044e56624062ba101152dcd1faab
meehaw1337-PC <15misiek12@wp.pl>
2020-12-09 00:43:32 +0100

    added format dropdown to message sender

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -5,7 +5,7 @@ import {Message as SemanticUIMessage} from "semantic-ui-react";
 export default function Message(props) {
     return (
         <div className={props.message.type === "incoming" ? "incomingMessage" : "outgoingMessage"}>
-            <SemanticUIMessage floating color={props.message.type === "outgoing" ? "black" : ""}>
+            <SemanticUIMessage floating color={props.message.type === "outgoing" ? "black" : undefined}>
                 <p>{props.message.timestamp.toLocaleTimeString()}</p>
                 <SemanticUIMessage.Header>{props.message.eventName}</SemanticUIMessage.Header>
                 <p>{props.message.messageContent}</p>
--- src/components/messagesender/MessageSender.js
+++ src/components/messagesender/MessageSender.js
@@ -1,30 +1,48 @@
 import React, {useState} from "react";
 import './MessageSender.css'
-import {Button, Form, Grid, GridColumn, Header, Input, Popup} from "semantic-ui-react";
-import TextareaAutosize from 'react-textarea-autosize';
+import {Button, Dropdown, Form, Grid, GridColumn, Header, Input, Menu, Popup, TextArea} from "semantic-ui-react";
 
 export default function MessageSender(props) {
     const [eventName, setEventName] = useState('');
     const [messageContent, setMessageContent] = useState('');
+    const [eventNameHistory, setEventNameHistory] = useState([]);
+    const [parseFailedMessage, setParseFailedMessage] = useState('');
+
+    const formats = [
+        {key: 1, text: 'Text', value: 'text'},
+        {key: 2, text: 'JSON', value: 'json'}
+    ]
+
+    const [selectedFormat, setSelectedFormat] = useState(formats[0].value);
 
     const resetForm = () => {
         setEventName('');
         setMessageContent('');
+        setParseFailedMessage('');
     }
 
     const eventInputHandler = (event) => setEventName(event.target.value);
     const contentInputHandler = (event) => setMessageContent(event.target.value);
     const resetButtonHandler = () => resetForm();
     const sendButtonHandler = () => {
-        props.socket.emit(eventName, messageContent);
-        props.messageSentHandler({eventName, messageContent, type: 'outgoing', timestamp: new Date()});
+        try {
+            const message = selectedFormat === 'text' ? messageContent : JSON.parse(messageContent);
+            props.socket.emit(eventName, message);
+            props.messageSentHandler({
+                eventName,
+                messageContent: JSON.stringify(messageContent),
+                type: 'outgoing',
+                timestamp: new Date()
+            });
 
-        if (!eventNameHistory.includes(eventName)) {
-            setEventNameHistory(eventNameHistory => [...eventNameHistory, eventName]);
+            if (!eventNameHistory.includes(eventName)) {
+                setEventNameHistory(eventNameHistory => [...eventNameHistory, eventName]);
+            }
+            setParseFailedMessage('');
+        } catch (error) {
+            setParseFailedMessage(`${error.name}: ${error.message}`);
         }
     };
-
-    const [eventNameHistory, setEventNameHistory] = useState([]);
 
     return (
         <div className="messageSender">
@@ -35,7 +53,13 @@ export default function MessageSender(props) {
                 {eventNameHistory.map(event => <option value={event}>{event}</option>)}
             </datalist>
             <Form className="margin">
-                <TextareaAutosize placeholder="Message" onChange={contentInputHandler} value={messageContent}/>
+                <Form.Field
+                    control={TextArea}
+                    onChange={contentInputHandler}
+                    placeholder="Message"
+                    error={parseFailedMessage === '' ? undefined : parseFailedMessage}
+                    value={messageContent}
+                />
             </Form>
             <Grid columns={2}>
                 <GridColumn>
@@ -45,6 +69,13 @@ export default function MessageSender(props) {
                 </GridColumn>
                 <GridColumn>
                     <div className="buttonRight margin">
+                        <div className="formatDropdown">
+                            <Menu compact>
+                                <Dropdown defaultValue={selectedFormat} options={formats}
+                                          onChange={(event, data) => setSelectedFormat(data.value)} fluid item
+                                          selection/>
+                            </Menu>
+                        </div>
                         {props.readyToSend ?
                             <div>
                                 <Button size="large" color="teal" onClick={sendButtonHandler}>Send</Button>
