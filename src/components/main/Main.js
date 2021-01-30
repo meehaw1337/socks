88db251276a65135543f4b12a04e80c939757a46
Michał Komar <michal.komar@miquido.com>
2021-01-30 18:10:57 +0100

    version tag added

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -64,6 +64,7 @@ export default function Main() {
                 <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a> from
                 <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
             </div>
+            <div className="footer">Version 1.1.0</div>
         </div>
     );
 }
